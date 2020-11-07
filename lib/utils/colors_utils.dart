968cdf7c3e62fa53db4e82a17f30b9d0e8a4f0b0
e2517 <carlos@achoweb.es>
2020-11-07 20:26:10 +0000

    colors

---
+++ lib/utils/colors_utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(65, 22, 85, 1);
+const kTextcolor = Color.fromRGBO(252, 223, 255, 1);
+const kDarkButton = Color.fromRGBO(255, 241, 253, 1);
