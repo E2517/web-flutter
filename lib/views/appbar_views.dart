13e2c0756a6e91cdb6d62a76e9f1f2f817e355c9
e2517 <carlos@achoweb.es>
2020-11-07 22:44:43 +0000

    responsive

--- lib/views/appbar_views.dart
+++ lib/views/appbar_views.dart
@@ -6,6 +6,70 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    print('dimension $size');
+
+    if (size.width < 700) {
+      return Container(
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(46),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, -2),
+              blurRadius: 30,
+              color: Colors.black.withOpacity(0.16),
+            ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  child: IconButton(
+                    icon: Icon(Icons.person),
+                    color: kPrimaryColor,
+                    onPressed: () {},
+                  ),
+                ),
+                SizedBox(width: 5),
+                Flexible(
+                  child: Text(
+                    'e2517',
+                    style: TextStyle(
+                        color: kPrimaryColor,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Flexible(
+                  flex: 0,
+                  child: MenuItem(
+                    title: "Login",
+                    press: () {},
+                  ),
+                ),
+                Flexible(
+                  child: DefaultButton(
+                    text: "Get Started",
+                    press: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
     return Container(
       margin: EdgeInsets.all(20),
       padding: EdgeInsets.all(20),
