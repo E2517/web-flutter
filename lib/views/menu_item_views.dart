8509b77200ac3413fd8aea06e7d3b50925dc1372
e2517 <carlos@achoweb.es>
2020-11-07 20:25:34 +0000

    menu

---
+++ lib/views/menu_item_views.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webflutter/utils/colors_utils.dart';
+
+class MenuItem extends StatelessWidget {
+  final String title;
+  final Function press;
+  const MenuItem({
+    Key key,
+    this.title,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Text(
+          title.toUpperCase(),
+          style: TextStyle(
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
